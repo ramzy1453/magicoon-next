1bd9d5f18b5ffd91bb3b02f1d470164d641470ea
ramzy1453 <kemmounramzy93@gmail.com>
2024-03-23 19:03:12 +0100

    cpus thread added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "build",
+  experimental: {
+    workerThreads: true,
+    cpus: 2,
+  },
 };
 
 module.exports = nextConfig;
